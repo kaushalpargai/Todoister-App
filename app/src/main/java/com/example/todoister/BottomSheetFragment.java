acf94266e98d565ff57c83c863d16fb5f625231f
kaushalpargai <kaushalpargai1.0@gmail.com>
2024-06-23 13:49:19 +0530

    changes in BottonSheetFragment

--- app/src/main/java/com/example/todoister/BottomSheetFragment.java
+++ app/src/main/java/com/example/todoister/BottomSheetFragment.java
@@ -25,7 +25,7 @@ import com.google.android.material.chip.Chip;
 import java.util.Calendar;
 import java.util.Date;
 
-public class BottomSheetFragment extends BottomSheetDialogFragment {
+public class BottomSheetFragment extends BottomSheetDialogFragment implements View.OnClickListener{
     private EditText enterTodo;
     private ImageButton calendarButton;
     private ImageButton priorityButton;
@@ -62,9 +62,11 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
         priorityRadioGroup=view.findViewById(R.id.radioGroup_priority);
 
         Chip todayChip=view.findViewById(R.id.today_chip);
+        todayChip.setOnClickListener(this);
         Chip tomorrowChip=view.findViewById(R.id.tomorrow_chip);
+        tomorrowChip.setOnClickListener(this);
         Chip nextweekChip=view.findViewById(R.id.next_week_chip);
-
+        nextweekChip.setOnClickListener(this);
 
         return view;
     }
@@ -101,5 +103,26 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
                         }
             }
         });
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id==R.id.today_chip){
+            //set data for today
+            calendar.add(Calendar.DAY_OF_YEAR,0);
+            dueDate=calendar.getTime();
+            Log.d("Time", "onClick: "+dueDate.toString());
+        }
+        else if (id==R.id.tomorrow_chip) {
+            calendar.add(Calendar.DAY_OF_YEAR,1);
+            dueDate=calendar.getTime();
+            Log.d("Time", "onClick: "+dueDate.toString());
+        } else if (id == R.id.next_week_chip) {
+            calendar.add(Calendar.DAY_OF_YEAR,7);
+            dueDate=calendar.getTime();
+            Log.d("Time", "onClick: "+dueDate.toString());
+        }
+
     }
 }
